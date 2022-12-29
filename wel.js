b50a6127c93752b50470e69f190ffe30fa115b87
k-vasanth09072002 <vasanthvasath74@gmail.com>
2022-12-29 10:08:16 +0530

    welcome-tocaletime-10times

---
+++ wel.js
@@ -0,0 +1,114 @@
+
+
+
+// const out=new Date();
+
+// function wel(){
+//     console.log(out.toLocaleTimeString());
+
+// }
+
+
+// setInterval(wel,3000);
+
+
+
+
+// const result = new Date();
+
+
+
+//   function out(){
+//       for(i=0;i<10;i++){
+//         document.write(result.toLocaleTimeString());
+//         document.write("<br>");
+//       }
+
+
+// }
+
+
+// setInterval(out,3000); 
+
+
+ //let cound="welcome";
+//const result = new Date();
+
+
+
+//   function out(){
+//       if(cound>10){
+//         clearInterval(result);
+//       }
+//       console.log(cound);
+
+//        }
+//      // document.write(i)
+//         document.write(result.toLocaleTimeString());
+//          document.write("<br>");
+      
+
+
+
+
+
+// setInterval(out,3000); 
+
+
+
+
+// let count=0;
+// const inter=setInterval(function(name){
+//     count++;
+
+//     if(count>10){
+//       clearInterval(inter);
+//     }
+
+//     console.log(count);
+
+// },1000,'gopi')
+
+
+
+
+
+
+
+
+
+
+
+
+let a=1;
+ let stop=setInterval(function(time){
+  a++;
+  if(a>10){
+    clearInterval(stop);
+  }
+  document.write(new Date().toLocaleTimeString()+"welcome");
+  document.write("<br>")
+
+ },3000)
+
+
+
+
+
+
+
+
+
+//dom//
+
+
+// function click(){
+
+//     document.getElementById("my").innerHTML="hello world!";
+
+// }
+// click();
+
+
+
+
